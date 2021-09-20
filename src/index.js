de81df7b98229bddb3a7576ce36c849d9fc058ee
dominggo1999 <arnolddominggo19@gmail.com>
2021-09-20 19:38:18 +0700

    Move Google Fonts Links

--- src/index.js
+++ src/index.js
@@ -22,11 +22,7 @@ ReactDOM.render(
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&family=Source+Sans+Pro:wght@400;600;700;900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=WindSong:wght@500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&family=Source+Sans+Pro:wght@400;600;700;900&family=WindSong:wght@500&display=swap"
             rel="stylesheet"
           />
         </Helmet>
