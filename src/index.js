662cbb5d4b60d7ca4e3e4e89cdc354b9a0dc250c
dominggo1999 <arnolddominggo19@gmail.com>
2021-09-20 20:15:30 +0700

    Add Website Logo Favicon

--- src/index.js
+++ src/index.js
@@ -11,21 +11,6 @@ ReactDOM.render(
   <React.StrictMode>
     <HelmetProvider context={helmetContext}>
       <Router>
-        <Helmet>
-          <link
-            rel="preconnect"
-            href="https://fonts.googleapis.com"
-          />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&family=Source+Sans+Pro:wght@400;600;700;900&family=WindSong:wght@500&display=swap"
-            rel="stylesheet"
-          />
-        </Helmet>
         <GlobalStyles />
         <App />
       </Router>
