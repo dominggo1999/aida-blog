4975521454f0d8e1735307271df5850a37c3f324
unknown <arnolddominggo19@gmail.com>
2022-04-19 19:20:25 +0700

    upgrade vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-import macrosPlugin from 'vite-plugin-babel-macros';
+import react from '@vitejs/plugin-react';
 
 import svgrPlugin from 'vite-plugin-svgr';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), macrosPlugin(), svgrPlugin({
-    svgrOptions: {
-      icon: true,
-      // ...svgr options (https://react-svgr.com/docs/options/)
-    },
-  })],
-  define: {
-    'process.platform': JSON.stringify('win32'),
-    'process.env': {},
-  },
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
+      },
+    }),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
 });
